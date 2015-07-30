3c915f7590217a956c64f0cf749f4b4f78c150aa
Ian MacDonald & Bob Ellis <ian@iancmacdonald.com>
2015-07-30 10:58:07 -0700

    receive post requests and renders template with message from that request

---
+++ index.js
@@ -0,0 +1,53 @@
+var Hapi = require('hapi');
+var http = require('http');
+var Path = require('path');
+
+var currentMessage = {};
+
+var server = new Hapi.Server();
+server.connection({ port: 3002 });
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: Path.join(__dirname, 'templates')
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply.view('index', { message: currentMessage.message });
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/incoming-snmp',
+  config: {
+
+    handler: function (request, reply) {
+      body = "";
+      request.payload.on('data', function (chunk) {
+        body += chunk;
+      });
+      request.payload.on('end', function (err) {
+        currentMessage = JSON.parse(body);
+        console.log(currentMessage.message);
+      });
+
+      // currentMessage = request.payload
+      // console.log(currentMessage);
+      reply("post received");
+    },
+
+    payload: {
+      output: 'stream',
+      parse: true
+    }
+  }
+})
+
+server.start(function(){
+  console.log('Server running at:', server.info.uri);
+});
