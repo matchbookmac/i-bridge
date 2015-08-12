08f2ae0d3b4c8f2989a6be7a588fe569bb25a3ec
Bob Ellis <ellisb@it0342mac-l.co.multnomah.or.us>
2015-08-12 11:26:32 -0700

    Add database writing and logging

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var
-  Hapi     = require('hapi'),
-  Path     = require('path')
+  Hapi    = require('hapi'),
+  Path    = require('path'),
+  wlog    = require('winston'),
+  mysql   = require('mysql'),
+  moment  = require('moment'),
+  bridgeOpenings = []
 ;
+
+
+wlog.add(wlog.transports.File, { filename: 'logs/winstonlog.log'});
 
 var server = new Hapi.Server();
 server.connection({ port: 80 });
@@ -37,6 +44,7 @@ server.views({
   path: Path.join(__dirname, 'public/templates')
 });
 
+
 server.route({
   method: 'GET',
   path: '/',
@@ -58,10 +66,17 @@ server.route({
 });
 
 server.route({
+  method: 'GET',
+  path: '/events',
+  handler: {
+    view: "events"
+  }
+});
+
+server.route({
   method: 'POST',
   path: '/incoming-snmp',
   config: {
-
     handler: function (request, reply) {
       body = "";
       request.payload.on('data', function (chunk) {
@@ -70,11 +85,64 @@ server.route({
       request.payload.on('end', function (err) {
         bridgeStatus = JSON.parse(body);
         var bridge = bridgeStatus.bridge;
+        console.log("bridge: " + bridge);
+        console.log("timestamp: " + bridgeStatus.timeStamp);
+        console.log("status: " + bridgeStatus.status);
         bridgeStatuses[bridge] = {
           status: bridgeStatus.status
         }
         console.log(bridgeStatuses);
         bridgeEventSocket.emit('bridge data', bridgeStatuses);
+//write data to AWS
+        connection = mysql.createConnection({
+          host     : 'uatgenrds.clvekbxagtjv.us-west-2.rds.amazonaws.com',
+          user     : 'uatbridgeapp',
+          password : '2HY4hykACYHmQK8g',
+          port     : 3306
+        })
+        connection.connect(function(err){
+          if (err) {
+            wlog.info("MYSQL connection error: " + err.code)
+            wlog.info("MYSQL connection error fatal?: " + err.fatal)
+          }
+          else{
+            wlog.info("MYSQL connection: successful. ")
+          }
+        });
+        bridgeName = bridgeStatus.bridge,
+        bridgeName = bridgeName.replace(/\'/g, ""),
+        timeStamp = moment(bridgeStatus.timeStamp).format("YYYY/MM/DD HH:mm:ss").toString();
+        if (bridgeStatus.status == false){
+          console.log("(false) bridgeStatus.status = " + bridgeStatus.status);
+          for (i = 0; i < bridgeOpenings.length; i++){
+            //check to see if there are any open bridge events that correspond with this close event
+            if (bridgeOpenings[i].name = bridgeName){
+              upTime = bridgeOpenings[i].uptime;
+              downTime = timeStamp;
+              //build sql string
+              var sql = 'INSERT INTO bridgeEvents (trainName, upTime, downTime) VALUES (' + "'" + bridgeName + "'" + ', ' + "'" + upTime + "'" + ', ' + "'" + downTime + "'" + ');';
+              connection.query(sql);
+              bridgeOpenings.splice(i, 1);
+            }
+          }
+
+        } else if (bridgeStatus.status == true) {
+          console.log("(true) bridgeStatus.status = " + bridgeStatus.status);
+
+          var bridgeEvent = {
+            name: bridgeName,
+            uptime: timeStamp
+          }
+          //check to see if there are any unclosed bridge openings, if so then delete them and replace with this new bridge opening
+          for (i = 0; i < bridgeOpenings.length; i++){
+            if(bridgeOpenings[i].name = bridgeName){
+              bridgeOpenings.splice(i, 1);
+            }
+          }
+          bridgeOpenings.push(bridgeEvent);
+        };
+
+
       });
 
       reply("post received");
