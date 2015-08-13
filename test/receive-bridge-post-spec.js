d068fbbce37cb62a1957be692b4179655553efca
Ian MacDonald <ian.macdonald@multco.us>
2015-08-12 17:46:12 -0700

    start setting up tests

---
+++ test/receive-bridge-post-spec.js
@@ -0,0 +1,15 @@
+var
+  expect      = require('chai').expect,
+  nock        = require('nock'),
+  receivePost = require('../modules/receive-bridge-post')
+;
+
+describe('receivePost', function () {
+  // before(function postingToABridge() {
+  //   var aBridge = nock('http://52.26.186.75:80')
+  //                   .post('/incoming-snmp')
+  //                   .reply(200, "post received")
+  //                 ;
+  // });
+  it('successfully receives post to a-bridge');
+});
