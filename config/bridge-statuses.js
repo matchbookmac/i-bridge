0b32090118428520757087ad53efa8b5f31bd058
Bob Ellis <ellisb@it0342mac-l.co.multnomah.or.us>
2015-08-13 13:04:47 -0700

    Refactoring module back to named function

---
+++ config/bridge-statuses.js
@@ -0,0 +1,20 @@
+module .exports = function () {
+  var bridgeStatuses = {
+    "cuevas crossing": {
+      status: false
+    },
+    hawthorne: {
+      status: false
+    },
+    broadway: {
+      status: false
+    },
+    burnside: {
+      status: false
+    },
+    morrison: {
+      status: false
+    },
+  };
+  return bridgeStatuses;
+}
