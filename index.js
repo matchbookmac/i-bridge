529a0b4392ecfefee24e6f6b82e617946ab76cc5
Ian MacDonald <ian.macdonald@multco.us>
2015-08-21 08:39:42 -0700

    Update hapi to > 9.0

--- index.js
+++ index.js
@@ -3,26 +3,33 @@ var
   Hapi           = require('hapi'),
   Path           = require('path'),
   fs             = require('fs'),
+  inert          = require('inert'),
+  vision         = require('vision'),
   util           = require('util'),
+  lout           = require('lout'),
   wlog           = require('winston'),
   port           = require('./config/config').port,
   bridgeStatuses = require('./config/config').bridges
 ;
 var options = {
-  port: 443,
-  tls: {
-    key: fs.readFileSync(Path.join(__dirname + '/keys/server.key')),
-    cert: fs.readFileSync(Path.join(__dirname + '/keys/server.crt')),
-    ca: fs.readFileSync(Path.join(__dirname + '/keys/server.csr'))
-  }
+  port: port
+  // tls: {
+  //   key: fs.readFileSync(Path.join(__dirname + '/keys/server.key')),
+  //   cert: fs.readFileSync(Path.join(__dirname + '/keys/server.crt'))
+  //   // ca: fs.readFileSync(Path.join(__dirname + '/keys/server.csr'))
+  // }
 };
-console.log(options);
+
 var server = new Hapi.Server();
 server.connection(options);
-var io     = require('socket.io')(server.listener);
+var io = require('socket.io')(server.listener);
 
 var bridgeEventSocket = io.on('connection', function (socket) {
   socket.emit('bridge data', bridgeStatuses);
+});
+
+server.register([inert, vision, lout], function (err) {
+  if (err) wlog.error(err);
 });
 
 server.views({
