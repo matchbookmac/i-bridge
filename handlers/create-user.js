10c649856dfd41c422df0dac737a2bb84b557311
Ian MacDonald <ian.macdonald@multco.us>
2015-09-02 14:34:32 -0700

    Add success page for registration

--- handlers/create-user.js
+++ handlers/create-user.js
@@ -12,12 +12,23 @@ module.exports = function (request, reply) {
     var token = buf.toString('hex');
     User.create({ email: email, token: token })
         .then(function (user) {
-          var response = reply(user);
+          var context = {
+            user: {
+              email: user.email,
+              token: user.token
+            }
+          };
+          var response = reply.view('new-user', context);
           response.created('/bridges');
         })
         .catch(function (err) {
           var error = boom.badRequest(err);
-          var context = { message: 'e-mail already taken, please try again'};
+          var context = {
+            message: {
+              text: 'e-mail already taken, please try again',
+              error: error
+            }
+          };
           var response = reply.view('new-user', context);
           response.location('/users/new');
         });
