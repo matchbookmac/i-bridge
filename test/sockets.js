5524a2ddba40425ffa7639c1add1b65018608e16
Ian MacDonald <ian.macdonald@multco.us>
2015-09-10 14:40:12 -0700

    Change to new prod database

--- test/sockets.js
+++ test/sockets.js
@@ -2,17 +2,18 @@ var socket = require('socket.io-client')('http://54.191.150.69');
 socket.on('bridge data', function (data) {
   console.log(data);
 });
-setInterval(beAnnoying, 2000);
-
-setTimeout(function () {
-  process.exit();
-}, 15000);
+// setInterval(beAnnoying, 2000);
+//
+// setTimeout(function () {
+//   process.exit();
+// }, 15000);
 
 function beAnnoying() {
   socket.emit('bridge data', { foo: 'bar'});
   console.log('beep');
   return;
 }
+beAnnoying();
 // setTimeout(function () {
 //   socket.disconnect();
 // }, 1800);
