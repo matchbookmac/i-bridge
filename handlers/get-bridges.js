9e3c3141d1b3beaf8e74ef30704c3cc7cd88f32d
Ian MacDonald <ian.macdonald@multco.us>
2015-09-18 21:04:24 -0700

    Unbreak bridges route

--- handlers/get-bridges.js
+++ handlers/get-bridges.js
@@ -1,18 +1,23 @@
 var _              = require('lodash');
 var db             = require('../models/index');
-var sequelize      = db.sequelize;
+var Bridge      = db.bridge;
 var wlog           = require('winston');
 
 module.exports = function (request, reply) {
-  sequelize.query('SELECT DISTINCT bridge FROM BridgeEvents', { type: sequelize.QueryTypes.SELECT })
-              .then(function (rows) {
-                var response = reply(_.map(rows, function (bridge) {
-                  return bridge.bridge;
-                }));
-                response.header('Access-Control-Allow-Origin', '*');
-              })
-              .catch(function (err) {
-                reply(err);
-                wlog.error('There was an error finding bridges: ' + err);
-              });
+  Bridge.findAll({
+    where: {
+      name: {
+        $not: null
+      }
+    }
+  }).then(function (rows) {
+    var response = reply(_.map(rows, function (bridge) {
+      return bridge.bridge;
+    }));
+    response.header('Access-Control-Allow-Origin', '*');
+  })
+  .catch(function (err) {
+    reply(err);
+    wlog.error('There was an error finding bridges: ' + err);
+  });
 };
