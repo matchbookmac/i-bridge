116b7f02a2b5a76ceb79711dfb6d63b4754b901e
Ian MacDonald <ian.macdonald@multco.us>
2015-09-21 14:57:27 -0700

    findBridge finds bridges by name and id

--- modules/find-bridge.js
+++ modules/find-bridge.js
@@ -3,19 +3,32 @@ var db             = require('../models/index');
 var Bridge         = db.bridge;
 
 module.exports = function findBridge(request, callback) {
-  var bridge = '%';
-  _.forIn(request.params.bridge.split(/[\W\d]+/), function (bridgeName) {
-    bridge += (bridgeName+'%');
-  });
-  Bridge.findOne({
-    where: {
-      name: {
-        $like: bridge
+  if (isNaN(parseInt(request.params.bridge))) {
+    var bridge = '%';
+    _.forIn(request.params.bridge.split(/[\W\d]+/), function (bridgeName) {
+      bridge += (bridgeName+'%');
+    });
+    Bridge.findOne({
+      where: {
+        name: {
+          $like: bridge
+        }
       }
-    }
-  }).then(function (bridge) {
-    callback(null, bridge);
-  }).catch(function (error) {
-    callback(err, null);
-  });
+    }).then(function (bridge) {
+      callback(null, bridge);
+    }).catch(function (error) {
+      callback(err, null);
+    });
+  } else {
+    var bridgeId = parseInt(request.params.bridge);
+    Bridge.findOne({
+      where: {
+        id: bridgeId
+      }
+    }).then(function (bridge) {
+      callback(null, bridge);
+    }).catch(function (err) {
+      callback(err, null);
+    });
+  }
 };
