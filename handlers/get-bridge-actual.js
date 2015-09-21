6e5aab3037d58efe4c5e1678d4469354a721d7a5
Ian MacDonald <ian.macdonald@multco.us>
2015-09-21 13:49:04 -0700

    Update get-bridge-actual for new schema

--- handlers/get-bridge-actual.js
+++ handlers/get-bridge-actual.js
@@ -1,5 +1,7 @@
-var              _ = require('lodash');
+var _              = require('lodash');
+var boom           = require('boom');
 var db             = require('../models/index');
+var Bridge         = db.bridge;
 var ActualEvent    = db.actualEvent;
 var logger         = require('../config/logging');
 
@@ -9,23 +11,30 @@ module.exports = function (request, reply) {
   _.forIn(request.params.bridge.split(/[\W\d]+/), function (bridgeName) {
     bridge += (bridgeName+'%');
   });
-  var params = {
-    order: 'upTime DESC',
+  Bridge.findOne({
     where: {
-      bridge: {
+      name: {
         $like: bridge
       }
     }
-  };
-  if (limit) params.limit = limit;
-  ActualEvent.findAll(params)
-              .then(function (rows) {
-                var response = reply(rows);
-                response.header('Access-Control-Allow-Origin', '*');
-              })
-              .catch(function (err) {
-                reply(err);
-                logger.error('There was an error finding bridge events for '+ bridge +': '+ err);
-              });
-
+  }).then(findActualEvents).catch(errorResponse);
+  function findActualEvents(bridge) {
+    var params = {
+      order: 'upTime DESC',
+      where: {
+        bridgeId: bridge.id
+      }
+    };
+    if (limit) params.limit = limit;
+    ActualEvent.findAll(params)
+      .then(function (rows) {
+        var response = reply(rows);
+        response.header('Access-Control-Allow-Origin', '*');
+      })
+      .catch(errorResponse);
+  }
+  function errorResponse(err) {
+    reply(boom.badRequest(err));
+    logger.error('There was an error finding bridge events for '+ bridge +': '+ err);
+  }
 };
